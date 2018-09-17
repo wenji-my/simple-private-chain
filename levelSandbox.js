99972fe482b9d33f8e8009b1bc673b4997dd4ff4
wenji-my <my841686869@163.com>
2018-09-17 23:22:42 +0800

    first commit

---
+++ levelSandbox.js
@@ -0,0 +1,68 @@
+/* ===== Persist data with LevelDB ===================================
+|  Learn more: level: https://github.com/Level/level     |
+|  =============================================================*/
+
+const level = require('level');
+const chainDB = './chaindata';
+const db = level(chainDB);
+
+// Add data to levelDB with key/value pair
+function addLevelDBData(key,value){
+  db.put(key, value, function(err) {
+    if (err) return console.log('Block ' + key + ' submission failed', err);
+  })
+}
+
+// Get data from levelDB with key
+function getLevelDBData(key){
+  db.get(key, function(err, value) {
+    if (err) return console.log('Not found!', err);
+    console.log('Value = ' + value);
+  })
+}
+// Add data to levelDB with value
+function addDataToLevelDB(key, value) {
+  let i = key;
+  let dataArray = [];
+    return new Promise((resolve, reject) => {
+      db.createReadStream().on('data', function(data) {
+            i++;
+            dataArray.push(data)
+          }).on('error', function(err) {
+              return console.log('Unable to read data stream!', err)
+          }).on('close', function() {
+            addLevelDBData(key, value)
+            resolve(dataArray)
+          });
+      
+    })
+}
+
+function getDataArray() {
+  let dataArray = [];
+  return new Promise((resolve, reject) => {
+    db.createReadStream().on('data', function(data) {
+          dataArray.push(data)
+        }).on('error', function(err) {
+            return console.log('Unable to read data stream!', err)
+        }).on('close', function() {
+          resolve(dataArray)
+        });
+    
+  })
+}
+
+/* ===== Testing ==============================================================|
+|  - Self-invoking function to add blocks to chain                             |
+|  - Learn more:                                                               |
+|   https://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/  |
+|                                                                              |
+|  * 100 Milliseconds loop = 36,000 blocks per hour                            |
+|     (13.89 hours for 500,000 blocks)                                         |
+|    Bitcoin blockchain adds 8640 blocks per day                               |
+|     ( new block every 10 minutes )                                           |
+|  ===========================================================================*/
+
+module.exports = {
+  addDataToLevelDB, getLevelDBData, getDataArray
+}
